7dcd4c2b9ef408eb1f424fa90dcde4c527ef5705
cpalaciosvSOEN <cpalaciosv@soen.pe>
2023-07-22 17:12:11 -0500

    feat(homepage): add banners

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+let sliderBanner = new $('#slider-banner').bxSlider({
+    captions: true,
+    pager: false,
+    auto: true,
+    pause: 3300,
+});
+
+let sliderNotices = new $('#slider-notices').bxSlider({
+    controls: false,
+    pager: false,
+    minSlides: 3,
+});
+
+
+
+// Path: js\index.js
