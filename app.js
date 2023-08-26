46bf4e05c8d4fd1a5f2d3d22bbbdd968229fe4a6
cpalaciosvSOEN <cpalaciosv@soen.pe>
2023-08-26 15:09:56 -0500

    fix(routes)

---
+++ app.js
@@ -0,0 +1,119 @@
+// 1.
+const nombre = "Christian";
+console.log(nombre);
+
+// 2.
+const edad = 23;
+console.log(edad);
+
+// 3.
+const esEstudiante = true;
+console.log(esEstudiante);
+
+// 4.
+const comidaFavorita = "Pizza";
+console.log(comidaFavorita);
+
+// 5.
+const numeroMagico = 7;
+console.log(numeroMagico);
+
+// 6.
+const tieneMascota = false;
+console.log(tieneMascota);
+
+// 7.
+const hobbies = "Leer, Cocinar, Viajar";
+console.log(hobbies);
+
+// 8.
+const temperatura = 25.5;
+console.log(temperatura);
+
+// 9.
+const tieneHermanos = true;
+console.log(tieneHermanos);
+
+// 10.
+const pais = "Venezuela";
+console.log(pais);
+
+// 11.
+const altura = 1.73;
+console.log(altura);
+
+// 12.
+const tieneLicencia = false;
+console.log(tieneLicencia);
+
+// 13.
+const libroFavorito = "Steve Jobs";
+console.log(libroFavorito);
+
+// 14.
+const horasDeSueno = 3;
+console.log(horasDeSueno);
+
+// 15.
+const tieneRedesSociales = true;
+console.log(tieneRedesSociales);
+
+// 16.
+const frutaPreferida = "Cambur";
+console.log(frutaPreferida);
+
+// 17.
+const edadMascota = null;
+console.log(edadMascota);
+
+// 18.
+const numeroDeAmigos = 0;
+console.log(numeroDeAmigos);
+
+// 19.
+const estaSoleado = false;
+console.log(estaSoleado);
+
+// 20.
+const ciudadActual = "Lima";
+console.log(ciudadActual);
+
+// 21.
+const distanciaEnKm = 0;
+console.log(distanciaEnKm);
+
+// 22.
+const tieneMascota2 = false;
+console.log(tieneMascota2);
+
+// 23.
+const peliculaFavorita = "Cara Cortada";
+console.log(peliculaFavorita);
+
+// 24.
+const numeroDeLibros = 15;
+console.log(numeroDeLibros);
+
+// 25.
+const estaLloviendo = false;
+console.log(estaLloviendo);
+
+// 26.
+const deportePreferido = null;
+console.log(deportePreferido);
+
+// 27.
+const duracionEnMinutos = 185;
+console.log(duracionEnMinutos);
+
+// 28.
+const tieneTatuajes = false;
+console.log(tieneTatuajes);
+
+// 29.
+const colorPreferido = "Negro";
+console.log(colorPreferido);
+
+// 30.
+const horarioDeEstudio = "18:00 - 20:00";
+console.log(horarioDeEstudio);
